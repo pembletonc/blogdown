0d9f336079e32bd7d32574a63c06d427e7938a69
Corey Pembleton <pembletonc@gmail.com>
2018-12-16 21:56:47 -0500

    Update media-monitoring.R

--- content/dev-eval/media-monitoring.R
+++ content/dev-eval/media-monitoring.R
@@ -1,12 +1,6 @@
 library(rtweet)
 library(tidyverse)
 
-create_token(app = "my_twitter_app",
-             consumer_key = "slfkHXdbamCcqhqKEQaCHPBG6" ,
-             consumer_secret = "cZGc4GAXoM8eqsgiGS8p1UJa9ecvGcC3kfAnSxw16WpHPrdA7h",
-             access_token = "2651047838-e2eMzRfExP2oe3Rax4Cn1lI9rmUJ05VfACNtA7g",
-             access_secret = "niV33rv5tFXviRqIahvB6eKPEqyeBIKoQVAZTexC2wbFS")
-
 
 rt <- search_tweets("evaluation", n = 5000, include_rts = FALSE,
                     retryonratelimit = FALSE)
